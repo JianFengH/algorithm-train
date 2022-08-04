88dc3a487f69b4c4f47b971b85169c427399d314
JianFengH <707156268@qq.com>
2022-08-03 22:32:22 -0700

    Execution order: Promise callback -> then callback -> setTimeout callback

--- js/event_loop.js
+++ js/event_loop.js
@@ -5,13 +5,24 @@ setTimeout(() => {
 
   new Promise((resolve, reject) => {
     console.log(5);
+    resolve();
+  }).then(() => {
+    console.log(9);
   });
+
+  console.log(7);
 });
 
 console.log(3);
 
 new Promise((resolve, reject) => {
   console.log(4);
+  resolve();
+}).then(() => {
+  console.log(8);
 });
 
-// 1 3 4 2 5
+console.log(6);
+
+// 1 3 4 6 8  
+// 2 5 7 9
