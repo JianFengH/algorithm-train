f785ff55e957cbc9a6a9dbb167b1ab93422fa618
JianFengH <707156268@qq.com>
2022-08-08 17:20:54 -0700

    Add insertion_sort

--- sort.js
+++ sort.js
@@ -1,12 +1,12 @@
 /**
  * pointer to last index as the largest one for ourter loop
  * pointer to first index of unsorted array for inner loop
- * @param {float} entry 
+ * @param {float} input 
  */
-export function bubble_sort(entry) {
-  const re = [].concat(entry);
-  for (let i = re.length - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
+export function bubble_sort(input) {
+  const re = [].concat(input);
+  for (let i = re.length - 1; i > 0; i--) { // sorted 
+    for (let j = 0; j < i; j++) { // unsorted
       if (re[j] > re[i]) {
         const t = re[i];
         re[i] = re[j];
@@ -19,4 +19,25 @@ export function bubble_sort(entry) {
 
 // const test1 = [10, 2, 5, 7, 2];
 // console.log(bubble_sort(test1));
+
+/**
+ * two sections: 1. sorted list 2. unsorted list
+ * one pointer 
+ */
+export function insertion_sort(input) {
+  const re = [].concat(input);
+  for (let i = 1, len = re.length; i < len; i++) { // unsorted
+    for (let j = 0; j < i; j++) { // sorted
+      const t = re[i];
+      if (t <= re[j]) { // change position
+        for (let m = i; m > j; m--) {
+          re[m] = re[m - 1];
+        }
+        re[j] = t;
+        break;
+      }
+    }
+  }
+  return re;
+}
 
--- test/sort.test.js
+++ test/sort.test.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { bubble_sort } from '../sort.js';
+import { bubble_sort, insertion_sort } from '../sort.js';
 
 describe('Sort algorithms', function () {
   const entry = [10, 2, 5, 7, 2];
@@ -8,6 +8,12 @@ describe('Sort algorithms', function () {
   describe('#bubble_sort()', function () {
     it(`should return sorted array: ${actual}`, function () {
       assert.equal(bubble_sort(entry).join(), actual.join());
+    });
+  });
+
+  describe('#insertion_sort()', function () {
+    it(`should return sorted array: ${actual}`, function () {
+      assert.equal(insertion_sort(entry).join(), actual.join());
     });
   });
 })
