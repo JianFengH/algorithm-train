6127f450ebdbb11f4896374c47b31f7ff9482614
JianFengH <707156268@qq.com>
2022-08-08 17:29:22 -0700

    Pull target to ourter loop

--- sort.js
+++ sort.js
@@ -27,9 +27,9 @@ export function selection_sort(input) {
 export function insertion_sort(input) {
   const re = [].concat(input);
   for (let i = 1, len = re.length; i < len; i++) { // unsorted
+    const t = re[i];
     for (let j = 0; j < i; j++) { // sorted
-      const t = re[i];
-      if (t <= re[j]) { // change position
+      if (t <= re[j]) { // it is the postion to insert
         for (let m = i; m > j; m--) {
           re[m] = re[m - 1];
         }
